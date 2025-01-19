fac4b56d787acc5a67b2e796880eee45804f2ee0
malaytrivedi555 <malaytrivedi555@gmail.com>
2025-01-19 05:26:14 +0000

    variables add

---
+++ .devcontainer/01_basics/variables.js
@@ -0,0 +1,14 @@
+const accountId = 1072003;
+let accountEmail = "malay@google.com";
+var accountPassword = "12345";
+let accountState;
+accountCity = "Lucknow";
+
+accountEmail = "mt@gm.com";
+accountPassword = "212121";
+accountCity = "Bahraich";
+
+console.log(accountId);
+
+
+console.table({accountId, accountEmail, accountPassword, accountCity, accountState})
